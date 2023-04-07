1524e650e86d254a4686bc488e7f482f3e06ba63
Sahadat Hossen <sahadatjh@gmail.com>
2023-04-07 20:51:11 +0600

    modify to async await to seeder

--- seeder.js
+++ seeder.js
@@ -1,29 +1,21 @@
 const path = require('path');
 
-function init() {  
+async function init() {  
     const config = require('./src/config');
     config.initEnvironmentVariables();
 
     const sequelize = require('./src/config/lib/sequelize');
 
-    sequelize.query(`CREATE DATABASE IF NOT EXISTS ${process.env.DB_NAME}`)
-        .then(() => {
-            require('./src/modules/user/user.model');
-            require('./src/modules/service/service.model');
-            require('./src/modules/permission/permission.model');
-            require('./src/modules/permission/service-permission.model');
-            require('./src/modules/profile/profile.model');
-            sequelize.sync()
-                .then(() => {
-                    console.log("DB seed copleted!");
-                })
-                .catch((err) => {
-                    console.log(err);
-                })
-        })
-        .catch((err) => {
-            console.log(err);
-        })
+    await sequelize.query(`CREATE DATABASE IF NOT EXISTS ${process.env.DB_NAME};`);
+
+    require('./src/modules/user/user.model');
+    const Service =  require('./src/modules/service/service.model');
+    require('./src/modules/permission/permission.model');
+    require('./src/modules/permission/service-permission.model');
+    require('./src/modules/profile/profile.model');
+    await sequelize.sync();
+
+    console.log("DB seed copleted!");
 }
 
 init();
